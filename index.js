1fbd48aac775897fc28f65a80169eb086e3b108d
zmaniacz <zz@redial.net>
2016-08-05 01:43:50 -0700

    Lodash is useful as shit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import _ from 'lodash';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import 'react-virtualized/styles.css';
